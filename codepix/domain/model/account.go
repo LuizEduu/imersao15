06233a3773cb7065489ecb38415530f5acdd09ec
Luiz Eduardo <l.eduardo8316@gmail.com>
2023-10-16 18:15:16 -0300

    feat: create bank and account entities

---
+++ codepix/domain/model/account.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/google/uuid"
+)
+
+type Account struct {
+	Base      `valid:"required"`
+	OwnerName string `json:"owner_name" valid:"notnull"`
+	Bank      *Bank  `valid:"-"`
+	Number    string `json:"number" valid:"notnull"`
+}
+
+func (account *Account) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewAccount(ownerName string, bank *Bank, number string) (*Account, error) {
+	account := Account{
+		OwnerName: ownerName,
+		Bank:      bank,
+		Number:    number,
+	}
+
+	account.ID = uuid.New().String()
+	account.CreatedAt = time.Now()
+
+	err := account.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &account, nil
+}
---
+++ codepix/go.mod
@@ -0,0 +1,8 @@
+module github.com/luizeduu/imersao/codepix-go
+
+go 1.15
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+)
